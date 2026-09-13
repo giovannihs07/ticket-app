34fb02f27fb05b132b71f87685e99696bd0aaa4f
Giovanni Hernández <ghernandezs@unicartagena.edu.co>
2026-09-12 23:43:39 -0500

    Agregando serializers para las peticiones de la API

---
+++ backend/tickets/serializers.py
@@ -0,0 +1,20 @@
+# tickets/serializers.py
+from rest_framework import serializers
+from .models import Ticket, Comentario
+
+
+class ComentarioSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comentario
+        fields = ['id', 'titulo', 'descripcion', 'fecha_registro']
+        read_only_fields = ['id', 'fecha_registro']
+
+
+class TicketSerializer(serializers.ModelSerializer):
+    comentarios = ComentarioSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Ticket
+        fields = ['id', 'titulo', 'descripcion', 'categoria',
+                  'prioridad', 'estado', 'created_at', 'updated_at', 'comentarios']
+        read_only_fields = ['id', 'created_at', 'updated_at']
